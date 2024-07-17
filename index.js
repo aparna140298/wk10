7ae18de4c871620769f89edaeee64feb6f0b8a0f
aparna140298 <aparna140298@gmail.com>
2024-07-17 11:44:08 -0400

    first change

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
